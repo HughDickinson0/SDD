6de72c93f5ab8e9098253e972c3d07dd9b493941
HughDickinson0 <109567385+HughDickinson0@users.noreply.github.com>
2022-08-04 16:46:40 +1000

    working game

--- SDD-main/220722.py
+++ SDD-main/220722.py
@@ -19,7 +19,10 @@ road_width = 460
 
 oil = pygame.image.load('oil_1.png')
 groundImg = pygame.image.load('ground.png')
-cactusImg = pygame.image.load('cactus_1.png')
+cactusImg0 = pygame.image.load('cactus_0.png')
+cactusImg1 = pygame.image.load('cactus_1.png')
+cactusImg2 = pygame.image.load('cactus_2.png')
+cactusImg3 = pygame.image.load('cactus_3.png')
 roadImg_0 = pygame.image.load('road_0.png')
 roadImg_1 = pygame.image.load('road_1.png')
 #importing obstacle images and ground and road
@@ -45,8 +48,18 @@ def things(thingx, thingy, thingw, thingh, color):
 #def rocks(rockx, rocky, rockw, rockh):
 #    gameDisplay.blit((
 
-def cactus(cactusx, cactusy, cactusw, cactush, color):
-    gameDisplay.blit((cactusImg),[cactusx, cactusy, cactusw, cactush])
+def cactus0(cactus0x, cactus0y, cactus0w, cactus0h, color):
+    gameDisplay.blit((cactusImg0),[cactus0x, cactus0y, cactus0w, cactus0h])
+
+def cactus1(cactus1x, cactus1y, cactus1w, cactus1h, color):
+    gameDisplay.blit((cactusImg1),[cactus1x, cactus1y, cactus1w, cactus1h])
+
+def cactus2(cactus2x, cactus2y, cactus2w, cactus2h, color):
+    gameDisplay.blit((cactusImg2),[cactus2x, cactus2y, cactus2w, cactus2h])
+
+def cactus3(cactus3x, cactus3y, cactus3w, cactus3h, color):
+    gameDisplay.blit((cactusImg3),[cactus3x, cactus3y, cactus3w, cactus3h])
+
 #cactus obstacle
 
 def road0(roadx,roady):
@@ -118,13 +131,31 @@ def game_loop():
     thing_width = 64
     thing_height = 64
 
-    cactus_startx = random.randrange(0, 175)
-    cactus_starty = -500
-    cactus_speed = 10
-    cactus_width = 78
-    cactus_height = 78
+    cactus0_startx = random.randrange(0, 175)
+    cactus0_starty = -570
+    cactus0_speed = 10
+    cactus0_width = 78
+    cactus0_height = 78
 
-    thingCount = 1
+    cactus2_startx = random.randrange(0, 175)
+    cactus2_starty = -500
+    cactus2_speed = 10
+    cactus2_width = 78
+    cactus2_height = 78
+
+    cactus3_startx = random.randrange(0, 175)
+    cactus3_starty = -658
+    cactus3_speed = 10
+    cactus3_width = 78
+    cactus3_height = 78
+
+    cactus1_startx = random.randrange(0, 175)
+    cactus1_starty = -712
+    cactus1_speed = 10
+    cactus1_width = 78
+    cactus1_height = 78
+
+    thingCount = 2
 
     #car(carx,cary)
 
@@ -172,8 +203,15 @@ def game_loop():
         things(thing_startx, thing_starty, thing_width, thing_height, black)
         thing_starty += thing_speed
 
-        cactus(cactus_startx, cactus_starty, cactus_width, cactus_height, black)
-        cactus_starty += cactus_speed
+        cactus0(cactus0_startx, cactus0_starty, cactus0_width, cactus0_height, black)
+        cactus1(cactus1_startx, cactus1_starty, cactus1_width, cactus1_height, black)
+        cactus2(cactus2_startx, cactus2_starty, cactus2_width, cactus2_height, black)
+        cactus3(cactus3_startx, cactus3_starty, cactus3_width, cactus3_height, black)
+        
+        cactus0_starty += cactus0_speed
+        cactus1_starty += cactus1_speed
+        cactus2_starty += cactus2_speed
+        cactus3_starty += cactus3_speed
         
         car(carx,cary)
 
@@ -190,16 +228,31 @@ def game_loop():
         if carx > display_width - car_width or carx < 0:
             crash()
             
-        if cactus_starty > display_height:
-            cactus_starty = 0 - cactus_height
-            cactus_startx = random.randrange(0, 175)
-            cactus_speed += 0.7
+        if cactus0_starty > display_height:
+            cactus0_starty = 0 - cactus0_height
+            cactus0_startx = random.randrange(0, 150)
+            cactus0_speed += 1
+
+        if cactus1_starty > display_height:
+            cactus1_starty = 0 - cactus1_height
+            cactus1_startx = random.randrange(0, 150)
+            cactus1_speed += 1
+
+        if cactus2_starty > display_height:
+            cactus2_starty = 0 - cactus2_height
+            cactus2_startx = random.randrange(675,800)
+            cactus2_speed += 1
+
+        if cactus3_starty > display_height:
+            cactus3_starty = 0 - cactus3_height
+            cactus3_startx = random.randrange(675,800)
+            cactus3_speed += 1
 
         if thing_starty > display_height:
             thing_starty = 0 - thing_height
-            thing_startx = random.randrange(175,625)
+            thing_startx = random.randrange(175,600)
             dodged += 1
-            thing_speed += 0.7
+            thing_speed += 1
         #
         if cary < thing_starty+thing_height:
             #print('y crossover')
@@ -208,7 +261,16 @@ def game_loop():
               #  print('x crossover')
                 crash()
         ####
-            if carx > cactus_startx and carx < cactus_startx + cactus_width or carx+car_width > cactus_startx and carx + car_width < cactus_startx + cactus_width:
+            if carx > cactus0_startx and carx < cactus0_startx + cactus0_width or carx+car_width > cactus0_startx and carx + car_width < cactus0_startx + cactus0_width:
+                crash()
+
+            if carx > cactus1_startx and carx < cactus1_startx + cactus1_width or carx+car_width > cactus1_startx and carx + car_width < cactus1_startx + cactus1_width:
+                crash()
+
+            if carx > cactus2_startx and carx < cactus2_startx + cactus2_width or carx+car_width > cactus2_startx and carx + car_width < cactus2_startx + cactus2_width:
+                crash()
+
+            if carx > cactus3_startx and carx < cactus3_startx + cactus3_width or carx+car_width > cactus3_startx and carx + car_width < cactus3_startx + cactus3_width:
                 crash()
         
         pygame.display.update()
